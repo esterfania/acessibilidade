7c7890c4e4fb75a2b23977097b6deefb7bf9ee37
Ester <esterfania@users.noreply.github.com>
2020-09-23 20:01:47 -0300

    Adiciona modulo YesNoButton no modulo principal

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -3,8 +3,10 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.scss']
+  styleUrls: ['./app.component.scss'],
 })
 export class AppComponent {
   title = 'acessibilidade';
+  public value = 'teste';
+  public label = 'testeLabel';
 }
