c305b57a8ebff141badd580808abf9bdefe490fe
Ester Gama <esterfaniasantos@gmail.com>
2020-10-23 21:32:48 -0300

    Cria o componente modal

---
+++ src/app/shared/components/modal/modal.component.ts
@@ -0,0 +1,8 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-modal',
+  styleUrls: ['./modal.component.scss'],
+  templateUrl: './modal.component.html',
+})
+export class ModalComponent {}
