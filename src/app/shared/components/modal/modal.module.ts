09ed8388a100580d7fcfb6f9e4e007a8851c4d20
Ester Gama <esterfaniasantos@gmail.com>
2020-11-03 23:00:25 -0300

    Implementa a diretiva FocusBackDirective

--- src/app/shared/components/modal/modal.module.ts
+++ src/app/shared/components/modal/modal.module.ts
@@ -3,9 +3,10 @@ import { NgModule } from '@angular/core';
 import { ModalComponent } from './modal.component';
 import { ModalService } from './services/modal.service';
 import { FocusTrapModule } from '../../directives/focus-trap/focus-trap.module';
+import { FocusBackModule } from '../../directives/focus-back/focus-back.module';
 
 @NgModule({
-  imports: [CommonModule, FocusTrapModule],
+  imports: [CommonModule, FocusTrapModule, FocusBackModule],
   declarations: [ModalComponent],
   exports: [ModalComponent],
   providers: [ModalService],
---
+++ src/app/shared/directives/focus-back/focus-back.module.ts
@@ -0,0 +1,9 @@
+import { CommonModule } from '@angular/common';
+import { NgModule } from '@angular/core';
+import { FocusBackDirective } from './focus-back.directive';
+@NgModule({
+  imports: [CommonModule],
+  declarations: [FocusBackDirective],
+  exports: [FocusBackDirective],
+})
+export class FocusBackModule {}
