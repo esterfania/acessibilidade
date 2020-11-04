e07b0b73e287eedba987120e886fd0d470f267de
Ester Gama <esterfaniasantos@gmail.com>
2020-11-03 21:48:19 -0300

    Cria a diretiva FocusTrapDirective

--- src/app/shared/components/modal/modal.module.ts
+++ src/app/shared/components/modal/modal.module.ts
@@ -2,9 +2,10 @@ import { CommonModule } from '@angular/common';
 import { NgModule } from '@angular/core';
 import { ModalComponent } from './modal.component';
 import { ModalService } from './services/modal.service';
+import { FocusTrapModule } from '../../directives/focus-trap/focus-trap.module';
 
 @NgModule({
-  imports: [CommonModule],
+  imports: [CommonModule, FocusTrapModule],
   declarations: [ModalComponent],
   exports: [ModalComponent],
   providers: [ModalService],
